7b3b4eac05d247744ce8984576788ade9c363716
iubns <iubns@naver.com>
2022-07-23 09:50:50 +0900

    New: add data

--- data.js
+++ data.js
@@ -1,20 +1,10 @@
 image = {
-    준호: 'jun.jpeg',
-    은진: 'blank.png',
-    재택: 'blank.png',
-    여빈: 'blank.png',
-    수정: 'blank.png',
-    선영: 'blank.png',
-    세울: 'blank.png',
-    유완: 'blank.png',
-    지인: 'blank.png',
-    은서: 'blank.png',
-    경혜: 'blank.png',
+    준호: 'jun.jpeg', 
 }
 
 userData = {
     '22.07.22' : {
-        은진: 2,
+        은진: 3,
         재택: 3,
         여빈: 0,
         준호: 1,
@@ -25,5 +15,6 @@ userData = {
         지인: 0,
         은서: 1,
         경혜: 3,
+        유림: 2,
     },
 }
